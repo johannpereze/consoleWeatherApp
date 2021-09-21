0df41dfa664ef2468235999df4c806d07e2e4123
johannpereze <sebas.jsp@gmail.com>
2021-09-20 22:25:19 -0500

    Ya está la primera estructura y la primera pregunta funcionando

---
+++ models/busquedas.js
@@ -0,0 +1,18 @@
+class Busquedas {
+
+historial = ['Bogotá', 'Medellín', 'Caldas']
+
+constructor(){
+    //TODO: leeer DB si existe
+}
+
+async ciudad(lugar = ''){
+    //peticion http
+    console.log(lugar);
+
+    
+    return [];//retornar los lugares
+}
+}
+
+module.exports = Busquedas;
